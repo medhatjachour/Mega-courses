4fd7bbd8288c82d8b7e1b3fc74275dbdb152ef63
medhatjachour <53466991+medhatjachour@users.noreply.github.com>
2024-12-13 13:49:54 +0200

    feat: add course controller

---
+++ server/src/controllers/courseController.ts
@@ -0,0 +1,39 @@
+import { Request ,Response} from "express";
+import Course from "../models/courseModel";
+
+
+export const listCourses= async(
+    req:Request,
+    res:Response
+) :Promise<void> =>{
+    const {category} = req.query;
+    try{
+        const courses = category && category !=="all"? await Course.scan("category").eq(category).exec():await Course.scan().exec()
+        res.json({message:"success",data:courses})
+    }
+    catch (error){
+        res.status(500)
+        res.json({message:"error retrieving courses ",error})
+    }
+}
+
+
+export const getCourse= async(
+    req:Request,
+    res:Response
+) :Promise<void> =>{
+
+    const {courseId} = req.params;
+    try{
+        const course = await Course.get(courseId)
+        if(!course){
+            res.status(404).json({message:"not found"})
+            return
+        }
+        res.json({message:"success",data:course})
+    }
+    catch (error){
+        res.status(500)
+        res.json({message:"error retrieving course ",error})
+    }
+}
--- server/src/index.ts
+++ server/src/index.ts
@@ -16,12 +16,19 @@ if (!isProduction){
     dynamoose.aws.ddb.local()
 }
 const app = express ()
+// This middleware parses incoming requests with JSON payloads. It's a built-in middleware in Express and is based on body-parser
 app.use(express.json())
+// Helmet helps secure your Express apps by setting various HTTP headers. This line includes several security middleware functions provided by Helmet.
 app.use(helmet())
+// This line configures the Cross-Origin Resource Policy (CORP) to allow cross-origin requests. It's a part of Helmet to handle security around resource sharing.
 app.use(helmet.crossOriginResourcePolicy({policy:"cross-origin"}))
+// Morgan is a middleware for logging HTTP requests and responses. The "common" format provides a standard predefined logging format.
 app.use(morgan("common"))
+// This middleware parses incoming requests with JSON payloads, specifically using the body-parser library, even though express.json() does this by default now
 app.use(bodyParser.json())
+// This middleware parses incoming requests with URL-encoded payloads (form data). The extended: false option ensures that the query string library is used
 app.use(bodyParser.urlencoded({extended:false}))
+// This middleware enables Cross-Origin Resource Sharing (CORS) for your application, allowing it to handle requests from different origins.
 app.use(cors())
 
 // routes
